63ac4ffb98eb3979b5839abf0c577f0206906789
Uexcel2022 <excellenceudoka@gmail.com>
2024-01-18 02:14:57 +0100

    commit

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
-
 const app = require("./app");
+
+mongoose
+  .connect(process.env.CONN_STR, {
+    useNewUrlParser: true,
+  })
+  .then((conn) => {
+    console.log(conn);
+    console.log("connection to db is successful...");
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server listing on port " + port);
